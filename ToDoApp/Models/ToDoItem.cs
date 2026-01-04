36cad36b2b8a80d604caf595f4f12c0edbd7ceee
KhanyisoV <khanyisovabaza@gmail.com>
2026-01-04 12:01:45 +0200

    ToDoList Models

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        public int Id { get; private set; }
+    
+        public string Name { get; set; } = null!;
+        
+        public bool IsCompleted { get; private set; }
+        public string Description { get; set; } = null!;
+        public DateTime DateCompleted { get; protected set; }
+        public  DateTime DateCreated { get; private set; } = DateTime.Now;
+
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+            DateCompleted = DateTime.Now;
+        }
+        public void UpdateName(string newName)
+        {
+            
+            if(newName == null || newName.Trim() == "")
+            {
+                throw new ArgumentNullException(nameof(newName), "Name cannot be null.");
+            }
+            else
+            {
+                Name = newName;
+            }
+        }
+    
+        public void UpdateDescription(string newDescription)
+        { 
+
+            if (newDescription == null || newDescription.Trim() == "")
+            
+                throw new ArgumentNullException(nameof(newDescription), "Description cannot be null.");
+            
+            if(newDescription.Length > 500)
+            
+                
+            throw new ArgumentException("Description too long");
+            
+            
+            Description = newDescription;
+         
+
+        }
+
+    }
+}
